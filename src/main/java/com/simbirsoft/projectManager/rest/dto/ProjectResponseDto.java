8faba6030dd8c7b26e4a3103897f9340e4ef7a5f
DolgovEA <rpg-sigma@yandex.ru>
2021-04-21 17:30:15 +0400

    #1: создал файл DTO для ответа отклика проекта

---
+++ src/main/java/com/simbirsoft/projectManager/rest/dto/ProjectResponseDto.java
@@ -0,0 +1,63 @@
+package com.simbirsoft.projectManager.rest.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "Проект")
+public class ProjectResponseDto {
+
+    @Schema(description = "Название проекта")
+    private String name;
+
+    @Schema(description = "Статус проекта")
+    private String status;
+
+    @Schema(description = "Время старта проекта")
+    private LocalDateTime startTime;
+
+    @Schema(description = "Время релиза проекта")
+    private LocalDateTime releaseTime;
+
+    public ProjectResponseDto() {
+    }
+
+    public ProjectResponseDto(String name, String status, LocalDateTime startTime, LocalDateTime releaseTime) {
+        this.name = name;
+        this.status = status;
+        this.startTime = startTime;
+        this.releaseTime = releaseTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getReleaseTime() {
+        return releaseTime;
+    }
+
+    public void setReleaseTime(LocalDateTime releaseTime) {
+        this.releaseTime = releaseTime;
+    }
+}
