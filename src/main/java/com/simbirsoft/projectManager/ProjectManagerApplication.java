74485053824a68bae8553a3b47b48e4a37d182b3
Spectrum Project <noob.online@yandex.ru>
2021-04-22 14:30:07 +0400

    пофиксена ошибка с class not found при запуске программы

--- src/main/java/com/simbirsoft/projectManager/ProjectManagerApplication.java
+++ src/main/java/com/simbirsoft/projectManager/ProjectManagerApplication.java
@@ -2,9 +2,10 @@ package com.simbirsoft.projectManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectManagerApplication {
+public class ProjectManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagerApplication.class, args);
