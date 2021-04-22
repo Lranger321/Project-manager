3effad21e9e661dc385f0c1e10c34fcb5899fccf
Spectrum Project <noob.online@yandex.ru>
2021-04-22 19:08:07 +0400

    #2: переписаны обработчики запросов CRUD в контроллере проектов

--- src/main/java/com/simbirsoft/projectManager/controller/ProjectController.java
+++ src/main/java/com/simbirsoft/projectManager/controller/ProjectController.java
@@ -10,52 +10,85 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Tag(name = "Управление проектом")
 @RestController
-@RequestMapping("api/project")
+@RequestMapping("api")
 public class ProjectController {
 
-    private final Map<String, ProjectResponse> repository;
+    //временное хранилище
+    //TODO: настроить репозиторий для работы с БД
+    private final Map<Integer, ProjectResponse> repository;
 
-    public ProjectController(Map<String, ProjectResponse> repository) {
+    public ProjectController(Map<Integer, ProjectResponse> repository) {
         this.repository = repository;
     }
 
     public ProjectController() {
-        repository = null;
+        repository = new HashMap<>();
+    }
+
+    @Operation(summary = "Получить список проектов")
+    @GetMapping(value = "/projects")
+    public ResponseEntity<List<ProjectResponse>> getAllProjects() {
+        // TODO: запрашивать все экземпляры проектов из БД
+        if (repository == null) throw new ProjectNotFoundException();
+        ArrayList<ProjectResponse> responses = new ArrayList<>(repository.values());
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @Operation(summary = "Получить проект по id")
+    @GetMapping(value = "/projects/{id}")
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable("id") int id) {
+        // TODO: запрашивать проект по id из БД
+        //TODO: добавить выброс исключения, если запись не найдена
+        if (repository == null) throw new ProjectNotFoundException();
+        return ResponseEntity.ok(repository.get(id));
     }
 
     @Operation(summary = "Добавить проект")
     @PostMapping(value = "/projects")
     public ResponseEntity<ProjectResponse> addProject(@RequestBody ProjectRequest projectRequestDto) {
-        //TODO работа с БД
+        //TODO: добавлять запись в БД
+        ProjectResponse response = new ProjectResponse(
+                projectRequestDto.getName(),
+                projectRequestDto.getStatus(),
+                projectRequestDto.getStartTime(),
+                projectRequestDto.getReleaseTime());
+        repository.put(projectRequestDto.getId(), response);
+        return ResponseEntity.ok().body(response);
+    }
 
+    @Operation(summary = "Обновить данные о проекте")
+    @PutMapping(value = "/projects/{id}")
+    public ResponseEntity<ProjectResponse> updateProject(@RequestBody ProjectRequest projectRequestDto) {
+        //TODO: обновлять запись в БД
+        //TODO: добавить выброс исключения, если запись не найдена
+        ProjectResponse response = new ProjectResponse(
+                projectRequestDto.getName(),
+                projectRequestDto.getStatus(),
+                projectRequestDto.getStartTime(),
+                projectRequestDto.getReleaseTime());
+        repository.put(projectRequestDto.getId(), response);
         return ResponseEntity.ok().body(new ProjectResponse());
     }
 
-    @Operation(summary = "Обновить информацию о проекте")
-    @PutMapping(value = "/projects")
-    public ResponseEntity<ProjectResponse> setProject(@RequestBody ProjectRequest projectRequestDto) {
-        //TODO работа с БД
-
-        return ResponseEntity.ok().body(new ProjectResponse());
-    }
-
-    @GetMapping(value = "{key}")
-    public ResponseEntity<ProjectResponse> getSimpleDto(@PathVariable("key") String key) {
-        // TODO ?
-
-        if (repository == null) throw new ProjectNotFoundException();
-
-        return ResponseEntity.ok(repository != null ? repository.get(key) : null);
+    @Operation(summary = "Удалить проект")
+    @DeleteMapping(value = "/projects/{id}")
+    public ResponseEntity<Integer> deleteProject(@PathVariable("id") int id) {
+        //TODO: удалять запись из БД
+        //TODO: добавить выброс исключения, если запись не найдена
+        repository.remove(id);
+        return ResponseEntity.ok().body(id);
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity handleException(IOException e) {
-        //
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ProjectResponse> handleException(IOException e) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
 }
