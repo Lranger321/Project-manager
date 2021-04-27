379ee8b81b7fa2034edbb5aea59477347834d371
DolgovEA <rpg-sigma@yandex.ru>
2021-04-27 16:59:29 +0400

    Добавил аннотации и методы для TaskRequest

--- src/main/java/com/simbirsoft/projectManager/dto/request/TaskRequest.java
+++ src/main/java/com/simbirsoft/projectManager/dto/request/TaskRequest.java
@@ -1,15 +1,64 @@
 package com.simbirsoft.projectManager.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Date;
 
+@Schema(description = "Задача")
 public class TaskRequest {
 
+    @Schema(description = "Название задачи")
     private String name;
 
+    @Schema(description = "Описание задачи")
     private String description;
 
+    @Schema(description = "Время старта задачи")
     private Date startDate;
 
+    @Schema(description = "Время релиза задачи")
     private Date endDate;
+
+    TaskRequest() {
+    }
+
+    TaskRequest(String name, String description, Date startDate, Date endDate) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 
 }
